169af59a8ceda0e993ce8c615a2f935895adb4b3
Lakis-NEET <nasikiqbal@gmail.com>
2021-10-01 19:42:52 +0700

    Initial commit

---
+++ Assets/Scripts/UserProgressData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UserProgressData
+{
+    public double Gold = 0;
+    public List<int> ResourcesLeves = new List<int>();
+}
